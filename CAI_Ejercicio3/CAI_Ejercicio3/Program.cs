0f7824a266fa3d5a3e1f2eeab101dc7572e32ed5
Gala <galamagdalena@hotmail.com>
2020-03-20 18:48:00 -0300

    Ejercicio 3

---
+++ CAI_Ejercicio3/CAI_Ejercicio3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            int i;
+
+            Console.Write("Escribe una palabra: ");
+
+            palabra = Console.ReadLine();
+
+            char[] array = palabra.ToCharArray();
+
+            for (i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == array[array.Length - 1 - i])
+                {
+                    if (i == array.Length - 2)
+                    {
+                        Console.Write("Es palindromo ");
+                    }
+                    
+                } else  {
+                    if (i == array.Length - 2)
+                    {
+                        Console.Write("No es palindromo");
+                    }
+
+ 
+                }
+
+            }
+
+            
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
